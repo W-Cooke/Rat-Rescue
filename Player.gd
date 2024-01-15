ff8020a88f49464cac9abd1a7589cb917604df32
Will Cooke <102586506+W-Cooke@users.noreply.github.com>
2024-01-15 21:41:38 +0000

    adding comments & tidying player code

--- Player.gd
+++ Player.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 
-
+#region Variables
 @export var SPEED = 150.0
 signal net_spin
 
 # spinning script variables
+@export_category ("Spin Variables")
 @export var angle_threshold : float = 200.0
 @export var array_size : int = 20
 var current_angle = 0.0
@@ -18,7 +19,7 @@ enum {CLOCKWISE, ANTICLOCKWISE, NOT}
 @onready var net = $Net
 @onready var netsprite = $Net/NetSprite
 @onready var particles = $Net/GPUParticles2D
-
+#endregion
 
 func _physics_process(_delta):
 	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * SPEED
@@ -27,18 +28,25 @@ func _physics_process(_delta):
 	move_and_slide()
 	controller_angle()
 
+
+#region Input Handling
 func controller_angle():
+	# get stick vector
 	var stick_rotation : Vector2 = Vector2(Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y), Input.get_joy_axis(0, JOY_AXIS_RIGHT_X))
+	# reverse x axis stick rotation so the net's angle corresponds to the angle of the analogue stick
 	stick_rotation.x *= -1.0
+	# conditional for deadzones
 	if stick_rotation.length() > 0.3:
 		current_angle = rad_to_deg(stick_rotation.angle())
 		net.rotation = deg_to_rad(current_angle)
 		var angle_diff = previous_angle - current_angle
 		if (angle_diff >= angle_threshold) or (angle_diff <= -angle_threshold):
+			# ditches angle difference if above a certain threshold (angles in godot range from 0-180 on the right side, 0- -180 on the left)
 			pass
 		else:
 			angle_array.push_front(angle_diff)
 		if angle_array.size() > array_size:
+			# triggers every time the array gets pushed to a certain size, current size is 20 so that takes 20 frames to fill
 			var mean = calculate_mean(angle_array)
 			if mean < -10:
 				handle_bools(CLOCKWISE)
@@ -54,6 +62,7 @@ func controller_angle():
 
 # function to handle spinning state of net
 func handle_bools(state):
+	# I really wanted to use the match statement, this felt like a good use
 	match state:
 		CLOCKWISE:
 			spinning_anticlockwise = false
@@ -78,3 +87,4 @@ func calculate_mean(arr):
 		sum += i
 	var mean : float = sum / count
 	return mean
+#endregion
