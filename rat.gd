380a668f79a35cfae3a407d27d1058091e1780eb
Will Cooke <william.cooke89@gmail.com>
2024-01-19 16:35:22 +0000

    pathfinding coming along
    rats now follow node pathfollow to it's conclusion and then reset node progress at the end

--- rat.gd
+++ rat.gd
@@ -1,63 +1,74 @@
 extends CharacterBody2D
 
-@export var MAX_SPEED = 50
-@export var target : Node2D
+@export var MAX_SPEED : int = 300
+@export var path_speed : float = 0.006
 var direction : Vector2
-var runaway = true
+var runaway : bool = true
+var following_path : bool = false
 var path_ratio : float = 0.0
-var path_speed : float = 200
 
+@onready var player : Node2D = get_tree().get_first_node_in_group("player")
+@onready var target : Node2D = player
 @onready var navigation_agent := $NavigationAgent2D as NavigationAgent2D
-@onready var paths = get_tree().get_nodes_in_group("path")
-@onready var rat = $"."
+@onready var paths : Array = get_tree().get_nodes_in_group("path")
+@onready var rat : CharacterBody2D = $"."
+var path_follow
+
+func _ready():
+	print(paths)
 
 func _physics_process(delta):
 	if runaway:
 		direction = to_local(navigation_agent.get_next_path_position())
 		velocity = -direction * MAX_SPEED * delta
 	else:
-		#TODO: something here?
+		direction = to_local(navigation_agent.get_next_path_position())
 		velocity = direction * MAX_SPEED * delta
 	move_and_slide()
+	progress_ratio()
+	print_debug("runaway: " + str(runaway))
+	print_debug("following_path: " + str(following_path))
+	print_debug("target: " + str(target))
+
+func progress_ratio():
+	if not runaway:
+		if path_ratio < 1:
+			path_follow.progress_ratio = path_ratio
+			path_ratio += path_speed
+			if path_ratio >= 1:
+				runaway = true
+				following_path = false
+				path_follow.progress_ratio -= 1
+				path_ratio = 0.0
+	else:
+		if path_follow:
+			path_follow.progress_ratio = 0.0
+
+func set_target():
+	if runaway:
+		target = player
+	elif not following_path:
+		target = _nearest_path().get_child(0).get_child(0)
+		following_path = true
 
 func create_path():
+	set_target()
 	navigation_agent.target_position = target.global_position
 
-# Either ^ or v needs something to do with the runaway logic, i feel
-# probably create_path()
 func _on_timer_timeout():
 	create_path()
 
-
-func follow_path(path):
-	# TODO: write code to follow along path, set runaway bool to true at end of path
-	# pseudocode:
-	# The answer is RemoteTransform2D!
-	# TODO: wrap rat in node2D, see if that fixes what I'm wanting to dooooooo
-	# navigation_agent.target_position = path.global_positon (maybe need to specify progress ratio?)
-	# assign location to progress ratio 0.0
-	# iterate through progress ratio until ratio >= 1
-	# turn off following and restart regular pathfinding
-	# runaway = true
-	pass
-
-func path_progress():
-	#TODO:
-	# function for process that will update the progress ratio of the pathfollow2D
-	# hopefully...
-	# path progress might have to be part of script for path? 
-	# UGH but that's clunky and I'd have to manually do stuff. easier to call it from this script
-	pass
+func _nearest_path():
+	# intialises list of paths
+	var nearest_path = paths[0]
+	# finds nearest path
+	for path in paths:
+		if path.get_child(0).get_child(0).global_position.distance_to(rat.global_position) < nearest_path.get_child(0).get_child(0).global_position.distance_to(rat.global_position):
+			nearest_path = path
+	return nearest_path
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("rat") and runaway:
-		# switches off running
 		runaway = false
-		# intialises list of paths
-		var nearest_path = paths[0]
-		# finds nearest path
-		for path in paths:
-			if path.global_position.distance_to(rat.global_position) < nearest_path.global_position.distance_to(rat.global_position):
-				nearest_path = path
-		#sends pathfollow2D to follow path function
-		follow_path(nearest_path.get_child(0))
+		path_follow = _nearest_path().get_child(0)
+		path_follow.progress_ratio = 0.0
