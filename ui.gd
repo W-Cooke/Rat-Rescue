a80325b4825586495b9c65272fb063d8de88f311
Will Cooke <william.cooke89@gmail.com>
2024-01-23 20:38:05 +0000

    populating level with paths & areas

--- ui.gd
+++ ui.gd
@@ -4,3 +4,4 @@ extends Control
 @onready var rats : Array = get_tree().get_nodes_in_group("rat")
 @onready var number_of_rats = rats.size()
 @onready var rats_left_num = $VBoxContainer/HBoxContainer/RatsLeftNum
+@onready var victory_label = $VBoxContainer/VictoryLabel
