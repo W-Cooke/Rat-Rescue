22adf71c8cd6937b52bdb3de9ee03b339ffcebe2
Will Cooke <william.cooke89@gmail.com>
2024-02-19 18:24:34 +0000

    implemented runaway code, now working on wandering

--- Steering.gd
+++ Steering.gd
@@ -1,7 +1,9 @@
 extends Node
 
+#region steering
 const DEFAULT_MASS : float = 2.0
 const DEFAULT_MAX_SPEED : float = 400.00
+#endregion
 
 static func follow(
 		velocity : Vector2,
@@ -13,3 +15,16 @@ static func follow(
 	var desired_velocity := (target_position - global_position).normalized() * max_speed
 	var steering := (desired_velocity - velocity) / mass
 	return velocity + steering
+
+static func run_away(
+		velocity : Vector2,
+		global_position : Vector2,
+		target_position : Vector2,
+		max_speed := DEFAULT_MAX_SPEED,
+		mass := DEFAULT_MASS
+	) -> Vector2:
+	var desired_velocity := (global_position - target_position).normalized() * max_speed
+	var steering := desired_velocity - velocity
+	return velocity + steering
+
+
--- rat_independence.gd
+++ rat_independence.gd
@@ -1,26 +1,75 @@
 extends CharacterBody2D
 
-@export var max_speed : float = 500.0
+@export var max_speed : float = 300.0
+
+@onready var label = $Label
+enum {WANDER, RUNNING, FOLLOWING}
+var state = RUNNING
+
+const DISTANCE_THRESHOLD : float = 3.0
+#region populates an array with all available rat textures
 @export_category("Rat Sprites")
 @export var rat_sprite_1 : CompressedTexture2D
 @export var rat_sprite_2 : CompressedTexture2D
 @export var rat_sprite_3 : CompressedTexture2D
 @export var rat_sprite_4 : CompressedTexture2D
+@export var rat_sprite_5 : CompressedTexture2D
+@export var rat_sprite_6 : CompressedTexture2D
+@onready var sprite_array : Array = [rat_sprite_1, rat_sprite_2, rat_sprite_3, rat_sprite_4, rat_sprite_5, rat_sprite_6]
+#endregion
 
-@onready var sprite_array : Array = [rat_sprite_1, rat_sprite_2, rat_sprite_3, rat_sprite_4]
+#region wander 
+const WANDER_CIRCLE_RADIUS : int = 8
+const WANDER_RANDOMNESS : float = 0.2
+var wander_angle : float = 0.0
+#endregion
+
 
 func _ready():
-	var image = sprite_array[randi_range(0, 3)].get_image()
+	# select a random rat texture and applies it
+	var image = sprite_array[randi_range(0, sprite_array.size() - 1)].get_image()
 	var texture = ImageTexture.create_from_image(image)
 	$Sprite2D.texture = texture
 
 func _physics_process(delta):
-	var target_global_position: Vector2 = get_global_mouse_position()
-	
-	velocity = Steering.follow(
-		velocity,
-		global_position,
-		target_global_position,
-		max_speed
-		)
+	if Input.is_action_just_pressed("ui_accept"):
+		state += 1
+		if state > 2:
+			state = 0
+	match(state):
+		RUNNING:
+			run_from_mouse()
+		FOLLOWING:
+			follow_the_mouse()
+		WANDER:
+			wander_around()
 	move_and_slide()
+
+func follow_the_mouse():
+	label.text = "Following"
+	var target_global_position: Vector2 = get_global_mouse_position()
+	if global_position.distance_to(target_global_position) < DISTANCE_THRESHOLD:
+		velocity = Vector2.ZERO
+	else:
+		velocity = Steering.follow(
+			velocity,
+			global_position,
+			target_global_position,
+			max_speed
+			)
+
+func run_from_mouse():
+	label.text = "Running!"
+	var target_global_position: Vector2 = get_global_mouse_position()
+	if global_position.distance_to(target_global_position) < DISTANCE_THRESHOLD:
+		velocity = Vector2.ZERO
+	else:
+		velocity = Steering.run_away(
+			velocity,
+			global_position,
+			target_global_position,
+			max_speed
+			)
+
+func wander_around():
+	label.text = "Wandering"
