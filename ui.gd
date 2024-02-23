90b444d2559ea336407e4c565e290fda37a09edd
Will Cooke <william.cooke89@gmail.com>
2024-02-23 16:02:48 +0000

    refactoring and moving stuff around

--- ui.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-#TODO: UI STUFF
-@onready var rats : Array = get_tree().get_nodes_in_group("rat")
-@onready var number_of_rats = rats.size()
-@onready var rats_left_num = $VBoxContainer/HBoxContainer/RatsLeftNum
-@onready var victory_label = $VBoxContainer/VictoryLabel
