1e75ceae993357acef6f5f1d34d948b4bb81c782
Will Cooke <william.cooke89@gmail.com>
2024-03-11 16:45:07 +0000

    basic iu sized up and ready to be coded

--- Assets/UI/ui.gd
+++ Assets/UI/ui.gd
@@ -3,10 +3,24 @@ extends Control
 #TODO: UI STUFF
 @onready var rats : Array = get_tree().get_nodes_in_group("rat")
 @onready var number_of_rats = rats.size()
+@onready var player = get_tree().get_first_node_in_group("player")
 @onready var rats_left_num = $VBoxContainer/HBoxContainer/RatsLeftNum
 @onready var victory_label = $VBoxContainer/VictoryLabel
 @onready var controller_icon = $MarginContainer/Controller
 @onready var keyboard_icon = $MarginContainer/Keyboard
 
 func _ready():
-	rats_left_num = str(number_of_rats)
+	$".".size = get_viewport().get_visible_rect().size
+
+func _process(_delta):
+	if player.keyboard_controls:
+		controller_icon.hide()
+		keyboard_icon.show()
+	else:
+		controller_icon.show()
+		keyboard_icon.hide()
+	rats_left_num.text = str(number_of_rats)
+
+
+func _on_player_rat_capture():
+	number_of_rats -= 1
