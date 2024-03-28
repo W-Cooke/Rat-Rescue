78f9f1a783db9657879f7f6e5c7877be924a93e5
Will Cooke <william.cooke89@gmail.com>
2024-03-28 09:46:43 +0000

    credits implementation starts

---
+++ UI/credits.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var file = "res://credits.txt"
+@onready var credits_label = $CreditsLabel
+var screen_size
+
+func _ready():
+	credits_label.text = load_file(file)
+
+func load_file(file):
+	var f = FileAccess.open(file, FileAccess.READ)
+	var content = f.get_as_text()
+	return content
