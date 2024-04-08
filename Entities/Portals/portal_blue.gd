3b7f7fee9e37ce69342675043783d5acafd1e790
Will Cooke <william.cooke89@gmail.com>
2024-04-08 14:25:17 +0100

    still working on portals

--- Entities/Portals/portal_blue.gd
+++ Entities/Portals/portal_blue.gd
@@ -1,16 +1,36 @@
 extends AnimatedSprite2D
 
+@onready var poof_particles = $PoofParticles
+@onready var tp_sound = $Bwoop
 @onready var portals = get_tree().get_nodes_in_group("portal_blue")
-@onready var self_portal = self
 var target_portal
+@onready var cooldown_timer = $CooldownTimer
+var tp_cooldown : bool = true
+signal teleported
 
 func _ready():
 	print(portals)
-	for x in portals:
-		if portals[x] == self_portal:
-			portals.remove_at(x)
+	if portals[0] == self:
+		print("removed portal at index 0")
+		portals.remove_at(0)
+	elif portals[1] == self:
+		print("removed portal at index 1")
+		portals.remove_at(1)
 	target_portal = portals[0]
+	print("target portal: " + str(target_portal))
 
 func _on_area_2d_body_entered(body):
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and tp_cooldown:
+		teleported.emit()
+		cooldown_timer.start()
 		body.global_position = target_portal.global_position
+
+
+func _on_cooldown_timer_timeout():
+	tp_cooldown = true
+
+func _on_portal_blue_teleported():
+	tp_sound.play()
+	tp_cooldown = false
+	cooldown_timer.start()
+	poof_particles.emitting = true
