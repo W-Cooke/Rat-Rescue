f3a9fb5760632344f52391dc298ce7be26b2d12c
Will Cooke <102586506+W-Cooke@users.noreply.github.com>
2024-04-16 06:38:46 +0000

    beginning work on save system

---
+++ Save/SavedGame.gd
@@ -0,0 +1,21 @@
+class_name SavedGame
+extends Resource
+
+# TODO: finish this, integrate with, first the settings, then the general game
+
+@export var level_1_complete : bool
+@export var level_2_complete : bool
+@export var level_3_complete : bool
+@export var level_4_complete : bool
+@export var level_5_complete : bool
+
+@export var music_on : bool
+@export var sfx_on : bool
+@export var controller_used : bool
+
+
+func save_game():
+    pass
+
+func load_game():
+    pass
